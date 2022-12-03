98e6f7fc229f4954d4b3c92dc3a3beeaf6658705
Aziz Limonu <azizlimonu@gmail.com>
2022-12-03 13:53:04 +0800

    post route added

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,4 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const {
+	createPost,
+	getPost,
+	likePost,
+	commentPost,
+	deletePost,
+	updatePost,
+	deleteComment,
+	editComment,
+} = require('../controllers/post');
+// authorize
 
-router.post('/register',)
+// router for create and get post
+router.route('/')
+  .post(createPost);
+
+// router for update like post
+router.route('/like')
+  .patch(likePost);
+
+// router for delete and edit post
+router.route('/:id')
+  .delete(deletePost)
+  .patch(updatePost)
+
+// router for comment post
+router.route('/comment')
+  .post(commentPost)
+  .delete(deleteComment)
+  .patch(editComment);
+
+module.exports = router;
